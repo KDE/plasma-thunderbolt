b2b459e60b4d1ed075ee4f085339bc23008cc898
Ivan Čukić <ivan.cukic@kde.org>
2026-06-06 08:48:28 +0200

    Add missing qqml.h include
    qmlRegisterType and related functions live in qqml.h
    
    (cherry picked from commit 9cd757e1ebacf2f559306403345f813716d42c44)

--- src/kcm/kcm_bolt.cpp
+++ src/kcm/kcm_bolt.cpp
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
 
+#include <qqml.h>
+
 #include <KLocalizedString>
 #include <KPluginFactory>
 #include <KQuickConfigModule>
